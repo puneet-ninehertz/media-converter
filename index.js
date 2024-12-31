f89bede63b0ed3ab10fa4afc19045ce879968cbc
Puneet Gehlot <puneet.gehlot@ninehertzindia.com>
2024-12-31 16:33:50 +0530

    added express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 9800;
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+  
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+})
